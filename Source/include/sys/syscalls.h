7452fbbe57fe66b672ff7f54b0593b2320790c55
moon-coffee <194471010+moon-coffee@users.noreply.github.com>
2026-09-06 19:47:37 +0900

    Many updates

--- Source/include/sys/syscalls.h
+++ Source/include/sys/syscalls.h
@@ -148,6 +148,9 @@
 #define SYSCALL_UNIX_LISTENING         270ULL
 #define SYSCALL_DISPLAY_KMS_MIRROR     271ULL
 #define SYSCALL_DISPLAY_KMS_MIRROR_DIRTY 272ULL
+#define SYSCALL_MEMFD_FROM_SHM         273ULL
+/* Bytes reserved for a shared-memory object (Kernel/Core/syscall/Syscall_Main.h). */
+#define SYSCALL_SHARED_MEMORY_SIZE     274ULL
 
 #define OS_STATUS_OK             0LL
 #define OS_STATUS_NOT_FOUND     -2LL
