13ba3483ba2ac1df9e6f76ccd724d389dccd859f
moon-coffee <194471010+moon-coffee@users.noreply.github.com>
2026-09-23 21:35:04 +0900

    update syscall

--- Source/include/sys/syscalls.h
+++ Source/include/sys/syscalls.h
@@ -105,6 +105,7 @@
 #define SYSCALL_GETTID            158ULL
 #define SYSCALL_SET_TID_ADDRESS   159ULL
 #define SYSCALL_GETRANDOM         170ULL
+#define SYSCALL_FTRUNCATE         173ULL
 #define SYSCALL_RENAME            175ULL
 #define SYSCALL_FD_POLL           179ULL
 #define SYSCALL_FUTEX             180ULL
